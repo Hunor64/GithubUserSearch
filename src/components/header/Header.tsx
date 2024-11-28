226271efd454f0fd86156a422b0662c086ec31c5
hunor64 <nhunor06@gmail.com>
2024-11-28 10:43:56 +0100

    Add toggle button for dark mode in header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -11,12 +11,12 @@ const Header = (props: HeaderProps) => {
 	return (
 		<div className="header">
 			<h1 className="websiteName">devfinder</h1>
-			<button onClick={props.toggleDarkMode}>
-				{props.darkMode ? "DARK" : "LIGHT"}
+			<button className="toggleButton" onClick={props.toggleDarkMode}>
 				<img
-					src={props.darkMode ? DarkIcon : LightIcon}
-					alt={props.darkMode ? "Dark Icon" : "Light Icon"}
+					src={!props.darkMode ? DarkIcon : LightIcon}
+					alt={!props.darkMode ? "Dark Icon" : "Light Icon"}
 				/>
+				{!props.darkMode ? "DARK" : "LIGHT"}
 			</button>
 		</div>
 	)

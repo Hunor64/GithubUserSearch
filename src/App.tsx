fe6fe213bc69813268092c42bc75b7d96d51d308
hunor64 <nhunor06@gmail.com>
2024-11-28 10:13:58 +0100

    Refactor dark mode logic for improved readability and consistency

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,27 @@ import SearchBar from "./components/searchBar/SearchBar"
 import UserData from "./components/userData/UserData"
 
 const App = () => {
-  const [darkMode, setDarkMode] = useState(() => {
-  const savedMode = document.cookie.split('; ').find(row => row.startsWith('darkMode='));
-  return savedMode ? savedMode.split('=')[1] === 'true' : true;
-  });
+	const [darkMode, setDarkMode] = useState(() => {
+		const savedMode = document.cookie
+			.split("; ")
+			.find((row) => row.startsWith("darkMode="))
+		return savedMode ? savedMode.split("=")[1] === "true" : true
+	})
 
-  const toggleDarkMode = () => {
-  const newMode = !darkMode;
-  setDarkMode(newMode);
-  document.cookie = `darkMode=${newMode}; path=/; max-age=31536000`; // 1 year
-  }
+	const toggleDarkMode = () => {
+		const newMode = !darkMode
+		setDarkMode(newMode)
+		document.cookie = `darkMode=${newMode}; path=/; max-age=31536000` // 1 year
+	}
 
-  useEffect(() => {
-    document.body.className = darkMode ? "dark" : "light"
-  })
+	useEffect(() => {
+		document.body.className = darkMode ? "dark" : "light"
+	})
 
 	return (
 		<div className="app">
 			<div className="container">
-				<Header darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+				<Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
 				<SearchBar />
 				<UserData />
 			</div>

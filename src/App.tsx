ad366534c7b40d779f280c226e06ac3e87a7d262
hunor64 <nhunor06@gmail.com>
2024-11-28 10:12:00 +0100

    Implement dark mode toggle and update header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
+import { useEffect, useState } from "react"
 import Header from "./components/header/Header"
 import SearchBar from "./components/searchBar/SearchBar"
 import UserData from "./components/userData/UserData"
 
 const App = () => {
-  return (
-    <>
-      <Header />
-      <SearchBar />
-      <UserData />
-    </>
-  )
+  const [darkMode, setDarkMode] = useState(() => {
+  const savedMode = document.cookie.split('; ').find(row => row.startsWith('darkMode='));
+  return savedMode ? savedMode.split('=')[1] === 'true' : true;
+  });
+
+  const toggleDarkMode = () => {
+  const newMode = !darkMode;
+  setDarkMode(newMode);
+  document.cookie = `darkMode=${newMode}; path=/; max-age=31536000`; // 1 year
+  }
+
+  useEffect(() => {
+    document.body.className = darkMode ? "dark" : "light"
+  })
+
+	return (
+		<div className="app">
+			<div className="container">
+				<Header darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+				<SearchBar />
+				<UserData />
+			</div>
+		</div>
+	)
 }
 
 export default App
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,25 @@
 import "./Header.css"
+import LightIcon from "/images/lightIcon.svg"
+import DarkIcon from "/images/darkIcon.svg"
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
+type HeaderProps = {
+	darkMode: boolean
+	toggleDarkMode: () => void
+}
+
+const Header = (props: HeaderProps) => {
+	return (
+		<div className="header">
+			<h1 className="websiteName">devfinder</h1>
+			<button onClick={props.toggleDarkMode}>
+				{props.darkMode ? "DARK" : "LIGHT"}
+				<img
+					src={props.darkMode ? DarkIcon : LightIcon}
+					alt={props.darkMode ? "Dark Icon" : "Light Icon"}
+				/>
+			</button>
+		</div>
+	)
 }
 
 export default Header
